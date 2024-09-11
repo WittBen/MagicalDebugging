681ffb3863b4bac848985dd9509dde706803541a
WittBen <BenjaminWitt@web.de>
2024-09-11 18:25:21 +0200

    Add files via upload

---
+++ MagicalDebugging/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace MagicalDebugging
+{
+
+  [DebuggerDisplay("Full name: {FullName} City: {City} Country: {Country}")]
+  public class Person
+  {
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public string? FullName { get => $"{LastName} {FirstName}"; }
+    public string? City { get; set; }
+    public string? Country { get; set; }
+  }
+}
---
+++ MagicalDebugging/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using MagicalDebugging;
+
+List<Person> ListOfPerson = new List<Person>() 
+{
+  new Person(){ FirstName="Henry", LastName="Maske", City="Berlin", Country="DE" },
+  new Person(){ FirstName="Robert", LastName="Lewandowski", City="Poznan", Country="PL" },
+  new Person(){ FirstName="Manuel", LastName="Neuer", City="München", Country="DE" }
+};
+
+
+Console.WriteLine(ListOfPerson.Count);
